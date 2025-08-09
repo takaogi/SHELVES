9876ced251098991ba2653505464725a4c99ccf8
takaogi <seramitras@gmail.com>
2025-08-09 19:50:40 +0900

    変更内容の説明

--- phases/session_create.py
+++ phases/session_create.py
@@ -144,9 +144,9 @@ class SessionCreate:
         self.flags = progress_info.setdefault("flags", {})
         self.wid = (
             self.flags.get("worldview_id")
-            or self.ctx.state.worldview_id
-            or self.flags.get("worldview", {}).get("id")
+            or self.flags.get("worldview", {}).get("id", "")
         )
+
         self.state = ctx.state
         self.session_mgr = ctx.session_mgr
 
@@ -736,7 +736,7 @@ class SessionCreate:
 
     def _ask_scenario_direction(self) -> tuple[dict, str]:
         pc = self.flags.get("player_character", {})
-        wid = self.flags.get("worldview_id")
+        wid = self.wid
         worldview = self.ctx.worldview_mgr.get_entry_by_id(wid)
         is_sequel = "sequel_to" in self.flags
 
@@ -891,7 +891,7 @@ class SessionCreate:
         meta = self.flags.get("_scenario_meta", {})
         length = meta.get("length", "")
         pc = self.flags.get("player_character", {})
-        wid = self.flags.get("worldview_id")
+        wid = self.wid
         worldview = self.ctx.worldview_mgr.get_entry_by_id(wid)
 
         self.ctx.nouns_mgr.set_worldview_id(wid)
